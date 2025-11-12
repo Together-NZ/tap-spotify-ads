76cae30b66740e4a900d7809da26d88abfc1630e
Peter Wang <pw33537515@gmail.com>
2025-11-13 10:51:57 +1300

    change the model method for fetching geo location to incremental

--- centralize_data/transform/models/google_ads_search_geo_target.sql
+++ centralize_data/transform/models/google_ads_search_geo_target.sql
@@ -1,5 +1,5 @@
   {{ config(
-    materialized='table',
+    materialized='incremental',
 ) }}
 
 WITH reference AS (
@@ -9,3 +9,9 @@ geo_location AS (
     SELECT * FROM `together-internal.google_ads_geo_target_location_macthing.geoTargetConstant_location`
 )
 select ref.* from reference as ref left join geo_location as geo on geo.geoTargetConstant.resourceName = ref.segments_geo_target_most_specific_location
+
+{% if is_incremental() %}
+
+where _DATA_DATE > (select max(_DATA_DATE) from {{ this }})
+
+{% endif %}
