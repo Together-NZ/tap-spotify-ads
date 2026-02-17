9b8e0636eb4ae93e90225b9cce8419b952e4f248
Peter Wang <pw33537515@gmail.com>
2026-02-17 17:12:15 +1300

    map campaign group name as campaign name for campaing later than 2025-09-01

--- geely/transform/models/linkedin_no_creative__geely.sql
+++ geely/transform/models/linkedin_no_creative__geely.sql
@@ -2,7 +2,7 @@
     materialized='table',
 ) }}
 
-WITH daily_stats AS (SELECT
+WITH daily_stats_raw AS (SELECT
   SAFE_CAST(JSON_VALUE(data, '$.video_completions') AS INT64) AS video_completion,
   SAFE_CAST(JSON_VALUE(data, '$.comments') AS INT64) AS comments,
   JSON_VALUE(data, '$.cost_in_usd') AS cost_in_usd,
@@ -24,17 +24,31 @@ WITH daily_stats AS (SELECT
   TIMESTAMP(JSON_VALUE(data, '$.start_at')),
   "Pacific/Auckland"
   ) AS date,
-
+  _sdc_extracted_at AS updated_at,
 
   JSON_VALUE(data, '$.creative_id') AS creative_id,
+  ROW_NUMBER() OVER (PARTITION BY JSON_VALUE(data,'$.creative_id'),  JSON_VALUE(data, '$.start_at') ORDER BY _sdc_extracted_at DESC) AS row_num
 FROM
   `geely-main.linkedin_raw__geely.ad_analytics_by_creative`),
+  daily_stats AS (
+    SELECT * EXCEPT(row_num)  FROM daily_stats_raw WHERE row_num = 1
+  ),
 creative_campaign_link AS (
   SELECT
   SPLIT(JSON_VALUE(data,'$.campaign'),':')[3] AS campaign_id,
   SPLIT(JSON_VALUE(data,'$.id'),':')[3] AS creative_id_1
   FROM `geely-main.linkedin_raw__geely.creatives`
-),final AS (
+),
+campaign_group_campaign_link AS (
+  SELECT DISTINCT JSON_VALUE(data,'$.name') AS campaign_group_name,JSON_VALUE(data,'$.id') AS campaign_group_id,date(timestamp(JSON_VALUE(data,'$.last_modified_time')) ,'Pacific/Auckland')as modified_date, ROW_NUMBER() OVER(PARTITION BY JSON_VALUE(data,'$.id') ORDER BY JSON_VALUE(data,'$.last_modified_time') DESC) AS row_num
+   FROM `geely-main.linkedin_raw__geely.campaign_groups` 
+  ),
+distinct_campaign_group_campaign_link AS (
+  SELECT * FROM campaign_group_campaign_link WHERE modified_date > '2025-09-01' and row_num=1 and lower(campaign_group_name) like '%uow%'
+),
+
+
+final AS (
 SELECT *, ROW_NUMBER() OVER (PARTITION BY daily_stats.creative_id,date,creative_id_1) AS row_num FROM daily_stats LEFT JOIN creative_campaign_link ON
 daily_stats.creative_id = creative_campaign_link.creative_id_1 ),
 stats_by_creative_campaign_id AS (
@@ -46,11 +60,23 @@ campaign_stats AS (      SELECT
     JSON_VALUE(data, '$.objective_type') as campaign_type,
     JSON_VALUE(data, '$.totalBudget.amount') as campaign_budget,
     JSON_VALUE(data, '$.status') as campaign_status,
+    JSON_VALUE(data,'$.campaign_group_id') AS campaign_group_id,
     FORMAT_DATE('%F', DATETIME(TIMESTAMP_MILLIS(safe_cast(JSON_VALUE(data, '$.runSchedule.start') as int64)), "Pacific/Auckland")) AS campaign_start_date,
     FORMAT_DATE('%F', DATETIME(TIMESTAMP_MILLIS(safe_cast(JSON_VALUE(data, '$.runSchedule.end') as int64)), "Pacific/Auckland")) AS camapign_end_date,
     ROW_NUMBER() OVER (PARTITION BY JSON_VALUE(data, '$.id'),JSON_VALUE(data, '$.account_id'),FORMAT_DATE('%F', DATETIME(TIMESTAMP_MILLIS(safe_cast(JSON_VALUE(data, '$.runSchedule.start') as int64)), "Pacific/Auckland")), FORMAT_DATE('%F', DATETIME(TIMESTAMP_MILLIS(safe_cast(JSON_VALUE(data, '$.runSchedule.end') as int64)), "Pacific/Auckland")) ) as row_num
     from `geely-main.linkedin_raw__geely.campaigns`),
-deduplicated_campaign_data AS (SELECT * EXCEPT(row_num) FROM campaign_stats where row_num = 1)
+deduplicated_campaign_data AS (SELECT * EXCEPT(row_num) FROM campaign_stats where row_num = 1),
+joint_campaign_group as (
+  SELECT deduplicated_campaign_data.*,distinct_campaign_group_campaign_link.campaign_group_name
+  FROM deduplicated_campaign_data LEFT JOIN distinct_campaign_group_campaign_link ON deduplicated_campaign_data.campaign_group_id = distinct_campaign_group_campaign_link.campaign_group_id
+
+),
+update_campaign_name as (
+  select joint_campaign_group.* EXCEPT(campaign_name,campaign_group_name),
+  CASE WHEN campaign_group_name is null then campaign_name else campaign_group_name 
+  END AS campaign_name 
+  FROM joint_campaign_group
+)
 SELECT stat_creative.*, campaign_data.* EXCEPT(campaign_id) FROM 
-stats_by_creative_campaign_id AS stat_creative LEFT JOIN  deduplicated_campaign_data as campaign_data
+stats_by_creative_campaign_id AS stat_creative LEFT JOIN  update_campaign_name as campaign_data
 ON stat_creative.campaign_id = campaign_data.campaign_id 
