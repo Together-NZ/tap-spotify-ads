d23c4a622e4b54b1b8a29480afffae2ca9cdd326
Peter Wang <pw33537515@gmail.com>
2026-02-28 15:19:06 +1300

    add testing sh file for testing compilling and smoke test meltano tap

---
+++ amp/ci_test.sh
@@ -0,0 +1,14 @@
+# Test the dbt-bigquery compile command
+export DBT_BIGQUERY_DATASET="test_dataset"
+meltano invoke dbt-bigquery compile
+
+# Somke test the meltano tap
+meltano invoke tap-facebook --help || { echo "tap-facebook failed"; exit 1; }
+
+meltano invoke tap-linkedin-ads --help || { echo "tap-linkedin-ads failed"; exit 1; }
+
+meltano invoke tap-ttd --help || { echo "tap-ttd failed"; exit 1; }
+
+meltano invoke tap-dv360 --help || { echo "tap-dv360 failed"; exit 1; }
+
+meltano invoke tap-hivestack --help || { echo "tap-hivestack failed"; exit 1; }
