695d688b6932a9ab354a68d30b32197a58cdc5a4
Peter Wang <pw33537515@gmail.com>
2026-03-04 19:37:14 +1300

    add necessary dbt argument

--- centralize_data/ci_test.sh
+++ centralize_data/ci_test.sh
@@ -2,6 +2,9 @@
 
 # Test the dbt-bigquery compile command
 export DBT_BIGQUERY_DATASET="test_dataset"
+export DBT_BIGQUERY_AUTH_METHOD='service_account'
+export DBT_BIGQUERY_METHOD='service_account'
+
 meltano invoke dbt-bigquery compile
 
 # Somke test the meltano tap
