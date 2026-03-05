38d0c9ac8b3a25fbc9cd1ce21800ceee7c8b39d4
Peter Wang <pw33537515@gmail.com>
2026-03-05 23:55:51 +1300

    correct linkedin transformation

--- geely/transform/models/linkedin__lotus.sql
+++ geely/transform/models/linkedin__lotus.sql
@@ -29,7 +29,7 @@ WITH daily_stats_raw AS (SELECT
   JSON_VALUE(data, '$.creative_id') AS creative_id,
   ROW_NUMBER() OVER (PARTITION BY JSON_VALUE(data,'$.creative_id'),  JSON_VALUE(data, '$.start_at') ORDER BY _sdc_extracted_at DESC) AS row_num
 FROM
-  `geely-main.linkedin_raw__lotus.ad_analytics_by_creative`),
+    `geely-main.linkedin_raw__lotus.ad_analytics_by_creative`),
   daily_stats AS (
     SELECT * EXCEPT(row_num)  FROM daily_stats_raw WHERE row_num = 1
   ),
@@ -83,6 +83,40 @@ update_campaign_name as (
   END AS campaign_name 
   FROM joint_campaign_group
 )
-SELECT stat_creative.*, campaign_data.* EXCEPT(campaign_id) FROM 
+SELECT stat_creative.*, campaign_data.* EXCEPT(campaign_id) ,
+SPLIT(campaign_name,'_')[OFFSET(1)] AS campaign_descr,
+CASE 
+  WHEN ARRAY_LENGTH(SPLIT(campaign_name, '_')) >= 8 THEN SPLIT(campaign_name, '_')[OFFSET(7)] 
+  ELSE NULL
+END AS audience_name,
+CASE 
+        WHEN SPLIT (campaign_name,'_')[OFFSET(2)] LIKE '%SOCIAL%'
+        AND (
+            lower(creative_name) LIKE '%vid%'
+            OR lower(campaign_name) LIKE '%vid%'
+        ) THEN 'Social Video'
+        WHEN SPLIT (campaign_name,'_')[OFFSET(2)] LIKE '%SOCIAL%'
+        AND (
+            lower(creative_name) NOT LIKE '%vid%'
+            AND lower(campaign_name) NOT LIKE '%vid%'
+        )
+        THEN 'Social Display'
+        ELSE 'Other'
+  END AS media_format,
+CASE 
+  WHEN ARRAY_LENGTH(SPLIT(creative_name, '_')) >= 7 THEN SPLIT(creative_name, '_')[OFFSET(5)] 
+  ELSE NULL
+END AS ad_format_detail,
+CASE 
+  WHEN ARRAY_LENGTH(SPLIT(creative_name, '_')) >= 7 THEN SPLIT(creative_name, '_')[OFFSET(ARRAY_LENGTH(SPLIT(creative_name, '_')) -2 )] 
+  ELSE NULL
+END AS ad_format,
+CASE 
+  WHEN ARRAY_LENGTH(SPLIT(creative_name, '_')) >= 7 THEN SPLIT(creative_name, '_')[OFFSET(ARRAY_LENGTH(SPLIT(creative_name, '_')) -1 )] 
+  ELSE NULL
+END AS creative_descr,
+'Linkedin' as publisher
+
+FROM 
 stats_by_creative_campaign_id AS stat_creative LEFT JOIN  update_campaign_name as campaign_data
 ON stat_creative.campaign_id = campaign_data.campaign_id 
