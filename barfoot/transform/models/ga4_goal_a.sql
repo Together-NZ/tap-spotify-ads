28a616b667a739bb34a77d3ce7d6927bb8cba071
Peter Wang <pw33537515@gmail.com>
2026-03-13 01:14:04 +1300

    cast eventvalue into float before cast into string in ga4

--- barfoot/transform/models/ga4_goal_a.sql
+++ barfoot/transform/models/ga4_goal_a.sql
@@ -98,8 +98,8 @@ WITH deduplicated_data AS (
         JSON_VALUE(data, '$.sessionCampaignName'),
         JSON_VALUE(data, '$.sessionManualAdContent'),
         JSON_VALUE(data, '$.eventName'),
-        JSON_VALUE(data, '$.eventValue'),
-        JSON_VALUE(data, '$.eventCount')
+        CAST(SAFE_CAST(JSON_VALUE(data, '$.eventValue') AS FLOAT64) AS STRING),
+        CAST(SAFE_CAST(JSON_VALUE(data, '$.eventCount') AS FLOAT64) AS STRING)
       ORDER BY _sdc_extracted_at DESC
     ) AS row_num
 
