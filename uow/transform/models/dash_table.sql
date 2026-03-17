68bb28a9d264dd7eeeb48e36aef1d0ee1fbb4819
Peter Wang <pw33537515@gmail.com>
2026-03-18 12:16:46 +1300

    use video_play as video view in tiktok

--- uow/transform/models/dash_table.sql
+++ uow/transform/models/dash_table.sql
@@ -22,7 +22,7 @@ WITH dash_table AS (
     UNION ALL
 
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion
-           ,video_25_completion,video_50_completion,video_75_completion, video_views,
+           ,video_25_completion,video_50_completion,video_75_completion, video_play AS video_views,
            campaign_name, publisher, campaign_descr,  date(date) as date
     FROM `uowaikato-main.tiktok_transformed.tiktok_ads_join`
 
