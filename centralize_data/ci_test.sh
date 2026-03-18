06ce10591466621eee63f00bd790c39ca2b337a6
Peter Wang <pw33537515@gmail.com>
2026-03-18 16:27:29 +1300

    add testing command on dbt

--- centralize_data/ci_test.sh
+++ centralize_data/ci_test.sh
@@ -6,7 +6,9 @@ export DBT_BIGQUERY_AUTH_METHOD='service-account'
 export DBT_BIGQUERY_METHOD='service-account'
 export DBT_BIGQUERY_PROJECT='together-internal'
 
+meltano --environment staging invoke dbt-bigquery:test
 grep '^\s*- name: tap-' meltano.yml | sed 's/.*name: //'
+
 
 
 for tap in $(grep '^\s*- name: tap-' meltano.yml | sed 's/.*name: //'); do
