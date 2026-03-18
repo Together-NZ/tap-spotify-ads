ea26fc69a9bfbc18d080cddebdaa63896425e6ae
Peter Wang <pw33537515@gmail.com>
2026-03-19 09:36:59 +1300

    remove test_expected_sum macros

--- amp/transform/macros/test_expected_sum.sql
+++
@@ -1,8 +0,0 @@
-{% test expected_sum(model, column_name, expected_value) %}
-
-SELECT
-  ROUND(SUM({{ column_name }}), 2) AS actual_sum
-FROM {{ model }}
-HAVING ROUND(SUM({{ column_name }}), 2) != {{ expected_value }}
-
-{% endtest %}
