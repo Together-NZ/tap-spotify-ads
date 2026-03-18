58f29e3b615cd129e657e66f4447112725e210a1
Peter Wang <pw33537515@gmail.com>
2026-03-19 11:18:00 +1300

    adding conversion column

--- moe/transform/models/dash_table.sql
+++ moe/transform/models/dash_table.sql
@@ -3,38 +3,38 @@
 ) }}
 WITH dash_table AS (
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     FROM `moe-main.ttd_transformed.ttd_transformed`
 
     UNION ALL
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     
     FROM `moe-main.dv360_transformed.dv360_standard`  WHERE LOWER(campaign_name) NOT LIKE '%yt%'
     UNION ALL
        SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     
     FROM `moe-main.dv360_transformed.dv360_youtube`  WHERE LOWER(campaign_name) LIKE '%yt%'
     UNION ALL
      SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion
            ,video_25_completion,video_50_completion,video_75_completion, video_play as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     FROM `moe-main.tiktok_transformed.tiktok`
 
     UNION ALL
 
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_played AS video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,conversions as conversions
     FROM `moe-main.facebook_transformed.facebook` WHERE LOWER(campaign_name) LIKE '%moe%'
 
     UNION ALL
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     FROM `moe-main.snapchat_transformed.snapchat`
     UNION ALL
     SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_views,
-            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date
+            campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
     FROM `moe-main.linkedin_transformed.linkedin`
     UNION ALL
     select media_cost,impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, 
@@ -43,7 +43,7 @@ WITH dash_table AS (
         CAST(0 AS INT64) AS video_50_completion,
         CAST(0 AS INT64) AS video_75_completion,
         CAST(0 AS INT64) AS video_views,
-    campaign_name,  publisher, campaign_descr, creative_descr, date(date) as date,
+    campaign_name,  publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
 
 from `moe-main.cm360_transformed.cm360_direct_buy`
 UNION ALL
@@ -53,7 +53,7 @@ UNION ALL
         CAST(0 AS INT64) AS video_50_completion,
         CAST(0 AS INT64) AS video_75_completion,
         CAST(0 AS INT64) AS video_views,
-    campaign_name,  publisher, campaign_descr, creative_descr, date(date) as date
+    campaign_name,  publisher, campaign_descr, creative_descr, date(date) as date,null as conversions
 FROM `moe-main.reddit_transformed.reddit`
 UNION ALL 
       SELECT media_cost, impressions, CAST(0 AS INT64) AS clicks, 
@@ -69,7 +69,7 @@ UNION ALL
            
            campaign_name,publisher, campaign_descr, 
            creative_descr AS creative_descr, -- Convert array to string
-           date,
+           date,null as conversions
     FROM `moe-main.hivestack_transformed.hivestack`
 ),
 with_channel AS (
