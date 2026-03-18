24f1448537ae75533b32b8b8d58df7574f19047e
Peter Wang <pw33537515@gmail.com>
2026-03-19 10:48:12 +1300

    fix table input name

--- amp/transform/models/facebook.sql
+++ amp/transform/models/facebook.sql
@@ -4,7 +4,7 @@
 WITH {{facebook.daily_breakdown(source_name='facebook_raw', table_name='ads_insights')}},
 {{facebook.conversion_goal(source_name='facebook_raw', table_name='ad_sets')}},
 {{facebook.custom_conversions(source_name='facebook_raw', table_name='custom_conversions')}},
-{{facebook.joining(source_name='facebook_raw', table_name='ads')}},
+{{facebook.joining(source_name='facebook_raw', table_name='ads_insights')}},
 {{facebook.metric_calculation()}},
 {{facebook.ad_data(source_name='facebook_raw', table_name='ads')}},
 {{facebook.adset_data(source_name='facebook_raw', table_name='ad_sets')}},
