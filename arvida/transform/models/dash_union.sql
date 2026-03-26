fa5c3f2f1a4c3a7ca898fde4e33c4c68f3f43404
Peter Wang <pw33537515@gmail.com>
2026-03-27 09:04:00 +1300

    add subbrands input for dash union

--- arvida/transform/models/dash_union.sql
+++ arvida/transform/models/dash_union.sql
@@ -2,9 +2,9 @@
     materialized='table',
 ) }}
 WITH final_result AS (
-  {{ dash_table_general_process.dash_union_non_search(source_name='dash_union_non_search', table_name='dash_table') }}
+  {{ dash_table_general_process.dash_union_non_search(source_name='dash_union_non_search', table_name='dash_table',sub_brands=env_var('SUB_BRANDS', 'null')) }}
    UNION ALL 
-   {{ dash_table_general_process.dash_union_search(source_name='dash_union_search', table_name='dash_table_search') }}
+   {{ dash_table_general_process.dash_union_search(source_name='dash_union_search', table_name='dash_table_search',sub_brands=env_var('SUB_BRANDS', 'null')) }}
  )
 SELECT
   -- Replace publisher from table2 if matched, else keep original
