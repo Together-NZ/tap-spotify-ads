cb549b004085d4d8547eaa5945d3c9f814f2dcf4
Peter Wang <pw33537515@gmail.com>
2026-03-26 16:40:48 +1300

    update plan code

--- kiwirail/transform/models/dv360_youtube__interislander.sql
+++ kiwirail/transform/models/dv360_youtube__interislander.sql
@@ -1,4 +1,4 @@
 {{ config(
     materialized='table',
 ) }}
-{{dv360.dv360_youtube(source_name='dv360_raw__interislander', table_name='dv360_youtube',plan_code=env_var('PLAN_CODE_INTERISLANDER', 'iinz'))}}
+{{dv360.dv360_youtube(source_name='dv360_raw__interislander', table_name='dv360_youtube',plan_code=env_var('PLAN_CODE_INTERISLANDER', 'iil'))}}
--- kiwirail/transform/models/ga4_goal_channel__interislander.sql
+++ kiwirail/transform/models/ga4_goal_channel__interislander.sql
@@ -1,4 +1,4 @@
 {{ config(
     materialized='table',
 ) }}
-{{ ga4.ga4_goal_channel(source_name='dash_union__interislander', table_name='dash_union__interislander', plan_code=env_var('PLAN_CODE_INTERISLANDER', 'iinz'), ga4_goal_a_model='ga4_goal_a__interislander') }}
+{{ ga4.ga4_goal_channel(source_name='dash_union__interislander', table_name='dash_union__interislander', plan_code=env_var('PLAN_CODE_INTERISLANDER', 'iil'), ga4_goal_a_model='ga4_goal_a__interislander') }}
