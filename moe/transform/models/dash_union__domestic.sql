2444f0c21f6f00c87d1134a4b615a856f2d3119b
Peter Wang <pw33537515@gmail.com>
2026-03-27 15:26:39 +1300

    enforce seperation of domestic and international on moe

--- moe/transform/models/dash_union__domestic.sql
+++ moe/transform/models/dash_union__domestic.sql
@@ -3,5 +3,16 @@
         materialized='table',
     )
 }}
-SELECT * FROM `moe-main.dash_table.dash_union` WHERE (LOWER(campaign_name) LIKE '%nz%' OR LOWER(campaign_name) LIKE '%domestic%'
-or LOWER(campaign_name) LIKE '%new zealand%')
+SELECT * FROM `moe-main.dash_table.dash_union` WHERE ( LOWER(campaign_name) LIKE '%domestic%')
+UNION ALL
+SELECT * FROM `moe-main.dash_table.dash_union` WHERE LOWER(campaign_name) LIKE '%hivestack%'
+UNION ALL
+SELECT * FROM `moe-main.dash_table.dash_union` WHERE ((LOWER(campaign_name) LIKE '%nz%' OR LOWER(campaign_name) LIKE '%new zealand%') 
+AND (LOWER(campaign_name) NOT LIKE '%international%' 
+AND LOWER(campaign_name) NOT LIKE '%oversea%'))
+UNION ALL 
+SELECT * FROM `moe-main.dash_table.dash_union` WHERE (LOWER(campaign_name) NOT LIKE '%international%' 
+AND LOWER(campaign_name) NOT LIKE '%oversea%' AND LOWER(campaign_name) NOT LIKE '%hivestack%' 
+AND LOWER(campaign_name) NOT LIKE '%domestic%'
+AND LOWER(campaign_name) NOT LIKE '%nz%'
+AND LOWER(campaign_name) NOT LIKE '%new zealand%')
--- moe/transform/models/ga4_goal_channel__international.sql
+++ moe/transform/models/ga4_goal_channel__international.sql
@@ -1,4 +1,9 @@
 {{ config(
     materialized='table',
 ) }}
-SELECT * FROM `moe-main.ga4_transformed.ga4_goal_channel` WHERE not country = 'New Zealand'
+SELECT * FROM `moe-main.ga4_transformed.ga4_goal_channel` WHERE (
+    campaign_name IN (
+        SELECT DISTINCT campaign_name 
+        FROM `moe-main.dash_table__international.dash_union__international`
+    ) OR (campaign_name NOT LIKE '%moe%' AND country != 'New Zealand')
+)
