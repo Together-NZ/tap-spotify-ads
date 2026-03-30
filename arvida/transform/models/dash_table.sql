6a8b2fd85b94170211ced63c54433efad9bdf25e
Peter Wang <pw33537515@gmail.com>
2026-03-30 15:22:17 +1300

    apply youtube conversion changes to Arvida

--- arvida/transform/models/dash_table.sql
+++ arvida/transform/models/dash_table.sql
@@ -14,7 +14,7 @@ WITH dash_table AS (
     
     UNION ALL
         SELECT media_cost, impressions, creative_name,clicks,  audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, null as conversions
+           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, conversions as conversions
     FROM `arvida-main.dv360_transformed.dv360_youtube`
     UNION ALL
 
