415c7a820606b53eadffadd7374aa5d603159cc9
Peter Wang <pw33537515@gmail.com>
2026-04-14 17:10:12 +1200

    update dv360 standard, update ga4 incremental update logic

--- volvo/transform/models/dash_table__volvo.sql
+++ volvo/transform/models/dash_table__volvo.sql
@@ -2,93 +2,27 @@
     materialized='table',
 ) }}
 WITH dash_table AS (
-    SELECT media_cost, impressions, clicks,creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, null as conversions
-    FROM `volvo-main.ttd_transformed__volvo.ttd_transformed__volvo`
+    {{dash_table_general_process.ttd(source_name='ttd_transformed', table_name='ttd_transformed__volvo') }}
 
     UNION ALL
-    SELECT media_cost, impressions, clicks,creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, conversions
-    FROM `volvo-main.dv360_transformed__volvo.dv360_youtube__volvo` WHERE LOWER(campaign_name) NOT LIKE '%yt%'
+    {{dash_table_general_process.dv360_youtube(source_name='dv360_transformed', table_name='dv360_youtube__volvo') }}
     UNION ALL
-        SELECT media_cost, impressions, clicks,creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, null as conversions
-    FROM `volvo-main.linkedin_transformed__volvo.linkedin__volvo`
+    {{dash_table_general_process.linkedin(source_name='linkedin_transformed', table_name='linkedin__volvo') }}
     UNION ALL
 
-    SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion, video_25_completion as video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date, conversions
-    FROM `volvo-main.dv360_transformed__volvo.dv360_standard__volvo` 
+     {{ dash_table_general_process.dv360_standard(source_name='dv360_transformed', table_name='dv360_standard__volvo',yt_source_name='dv360_transformed',yt_table_name='dv360_youtube__volvo') }}
     UNION ALL
-           SELECT media_cost, impressions,clicks,
-              ad_name AS creative_name,  
-           --ARRAY_TO_STRING(media_format, ', ') AS media_format,   -- Convert array to string
-           audience_name, -- Convert array to string
-           ad_format AS ad_format,         -- Convert array to string
-           ad_format_detail AS ad_format_detail, 
-            CAST(0 AS INT64) AS video_completion,
-            CAST(0 AS INT64) AS video_25_completion,
-            CAST(0 AS INT64) AS video_50_completion,
-            CAST(0 AS INT64) AS video_75_completion,
-            CAST(0 AS INT64) AS video_views,
-           
-           campaign_name,publisher, campaign_descr, 
-            creative_descr,  -- Convert array to string
-           date, conversions
-
-    FROM `volvo-main.google_ads_search_transformed__volvo.google_ads_demand__volvo`
+    {{dash_table_general_process.google_ads(source_name='google_ads', table_name='google_ads_demand__volvo') }}
     
     UNION ALL
-        SELECT media_cost, impressions, CAST(0 AS INT64) AS clicks, 
-           creative_name,   -- Convert array to string
-           audience_name AS audience_name, -- Convert array to string
-           ad_format AS ad_format,         -- Convert array to string
-           ad_format_detail AS ad_format_detail, 
-            CAST(0 AS INT64) AS video_completion,
-            CAST(0 AS INT64) AS video_25_completion,
-            CAST(0 AS INT64) AS video_50_completion,
-            CAST(0 AS INT64) AS video_75_completion,
-            CAST(0 AS INT64) AS video_views,
-           
-           campaign_name,publisher, campaign_descr, 
-           creative_descr AS creative_descr, -- Convert array to string
-           date, null as conversions
-    FROM `volvo-main.hivestack_transformed__volvo.hivestack__volvo`
+    {{dash_table_general_process.hivestack(source_name='hivestack_transformed', table_name='hivestack__volvo') }}
     UNION ALL
-           SELECT media_cost, impressions,clicks,
-              ad_name AS creative_name,  
-           --ARRAY_TO_STRING(media_format, ', ') AS media_format,   -- Convert array to string
-           audience_name, -- Convert array to string
-           ad_format AS ad_format,         -- Convert array to string
-           ad_format_detail AS ad_format_detail, 
-            CAST(0 AS INT64) AS video_completion,
-            CAST(0 AS INT64) AS video_25_completion,
-            CAST(0 AS INT64) AS video_50_completion,
-            CAST(0 AS INT64) AS video_75_completion,
-            CAST(0 AS INT64) AS video_views,
-           
-           campaign_name,publisher, campaign_descr, 
-            creative_descr,  -- Convert array to string
-           date, conversions
-
-    FROM `volvo-main.google_ads_search_transformed__volvo.google_ads_demand_2__volvo`
+    {{dash_table_general_process.google_ads(source_name='google_ads', table_name='google_ads_demand_2__volvo') }}
     UNION ALL
-    -- Handling Google Ads arrays by converting them to strings
-
-    SELECT media_cost, impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, video_completion,video_25_completion,video_50_completion,video_75_completion,video_played AS video_views,
-           campaign_name, publisher, campaign_descr, creative_descr, date(date) as date,conversions
-    FROM `volvo-main.facebook_transformed__volvo.facebook__volvo`
+    {{dash_table_general_process.meta(source_name='facebook_transformed', table_name='facebook__volvo') }}
 
     UNION ALL
-    select media_cost,impressions, clicks, creative_name, audience_name, ad_format, ad_format_detail, 
-        CAST(0 AS INT64) AS video_completion,
-        CAST(0 AS INT64) AS video_25_completion,
-        CAST(0 AS INT64) AS video_50_completion,
-        CAST(0 AS INT64) AS video_75_completion,
-        CAST(0 AS INT64) AS video_views,
-    campaign_name,  publisher, campaign_descr, creative_descr, date(date) as date,
-        null as conversions
-from `volvo-main.cm360_transformed__volvo.cm360_direct_buy__volvo`
+    {{dash_table_general_process.cm360(source_name='cm360_transformed', table_name='cm360_direct_buy__volvo') }}
 ),
 with_channel AS (
 SELECT * EXCEPT (publisher,channel), 
--- volvo/transform/models/ga4_goal_a__volvo.sql
+++ volvo/transform/models/ga4_goal_a__volvo.sql
@@ -1,5 +1,7 @@
 {{ config(
-    materialized='table',
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
+    partition_by={'field': 'date', 'data_type': 'date'},
 ) }}
 
 {{ ga4.ga4_goal_a(source_name='ga4_raw__volvo', table_name='goal',dash_union_source_name='dash_union__volvo',dash_union_table_name='dash_union__volvo') }}
